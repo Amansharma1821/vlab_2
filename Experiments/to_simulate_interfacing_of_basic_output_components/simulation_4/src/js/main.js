419327b95c92a4f304a0274f0f68c13873b2937b
Priyam Singh <priyam0singh@gmail.com>
2022-11-15 12:07:07 +0530

    minor patch

--- Experiments/to_simulate_interfacing_of_basic_output_components/simulation_4/src/js/main.js
+++ Experiments/to_simulate_interfacing_of_basic_output_components/simulation_4/src/js/main.js
@@ -1,20 +1,16 @@
-function changepage() {
-  var x = document.getElementById("pagechanger").value;
-  if (x == 1) document.getElementById("sm1").click();
-  else if (x == 2) document.getElementById("sm2").click();
-  else if (x == 3) document.getElementById("sm3").click();
-  else if (x == 4) document.getElementById("sm4").click();
-  else document.getElementById("sm5").click();
-}
-
 let image_tracker = "off";
 function changePower() {
   let image = document.getElementById("ifimg");
   if (image_tracker == "off") {
     image.src = "./src/images/LCD_On.gif";
+    document.getElementById("pushbuttonPower").innerHTML = "Stop Simulation";
+    document.getElementById("pushbuttonPower").style.backgroundColor = "red";
     image_tracker = "On";
   } else {
     image.src = "./src/images/LCD_Off.png";
+    document.getElementById("pushbuttonPower").innerHTML = "Start Simulation";
+    document.getElementById("pushbuttonPower").style.backgroundColor =
+      "#009C4E";
     image_tracker = "off";
   }
 }
